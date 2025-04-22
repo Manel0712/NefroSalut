a678d1e2e0bfb24f890c83f670e52f01426aaef6
marcmc <marc.marza@epiaedu.cat>
2025-04-22 21:14:53 +0200

    pequeño canvio

---
+++ APP/NefroSalut/app/src/main/java/com/mariona/nefrosalut/RegisterFamiliarActivity.kt
@@ -0,0 +1,10 @@
+package com.mariona.nefrosalut
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+class RegisterFamiliarActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.register_familiar)
+    }
+}
