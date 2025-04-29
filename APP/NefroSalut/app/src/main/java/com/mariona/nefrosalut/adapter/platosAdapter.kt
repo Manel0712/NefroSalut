6efa9f2cf7c6b9c039615685c0d5376747009f63
Marionaaa22 <mariona.claros@mataro.epiaedu.cat>
2025-04-29 20:36:34 +0200

    Adapters

---
+++ APP/NefroSalut/app/src/main/java/com/mariona/nefrosalut/adapter/platosAdapter.kt
@@ -0,0 +1,32 @@
+package com.mariona.nefrosalut.adapter
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.mariona.nefrosalut.R
+import com.mariona.nefrosalut.models.Aliments
+
+class platosAdapter(private val aliments: List<Aliments>) : RecyclerView.Adapter<platosAdapter.ViewHolder>() {
+
+    class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+        val nombre: TextView = itemView.findViewById(R.id.cardNomDelPlat)
+        val categoria: TextView = itemView.findViewById(R.id.cardCategoriaPlat)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.card_platos, parent, false)
+        return ViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val alimento = aliments[position]
+        holder.nombre.text = alimento.alimento
+        holder.categoria.text = alimento.categoria
+    }
+
+    override fun getItemCount(): Int {
+        return aliments.size
+    }
+}
